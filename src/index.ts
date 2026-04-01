3a4755e9464d3b6eee810b9ec40fd628a197556b
Yeos <yeos_k@yahoo.com>
2026-04-02 00:26:54 +0700

    config env

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-﻿import express from 'express';
+import 'dotenv/config';
+import express from 'express';
 import cors from 'cors';
 import { authRouter } from './routes/auth';
 import { usersRouter } from './routes/users';
