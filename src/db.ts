57ac1151f4b606d1bae8ae470c74d49e1dc2b56b
Yeos <yeos_k@yahoo.com>
2026-04-04 12:51:21 +0700

    db path

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,8 @@ import Database from 'better-sqlite3';
 import path from 'path';
 import { generateId } from './utils/id';
 
-const DB_PATH = path.resolve(__dirname, '../fjm.db');
+const DB_PATH = process.env['DB_PATH'] ?? path.resolve(__dirname, '../fjm.db');
+
 export const db = new Database(DB_PATH);
 
 db.pragma('foreign_keys = ON');
